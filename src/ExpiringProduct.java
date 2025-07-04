722d65ad687cfd537a9422d7c500131e1853fe1e
Omar Shaaban <98961039+Omar-Fahim@users.noreply.github.com>
2025-07-04 18:47:04 +0300

    ExpiringProduct Class
    Added abstract expiring product class with the logic of expiry added

---
+++ src/ExpiringProduct.java
@@ -0,0 +1,31 @@
+
+public abstract class ExpiringProduct extends Product{
+    private LocalDate expirationDate;
+
+    public ExpiringProduct(String name, double price, int quantity, LocalDate expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public boolean isExpired() {
+        // Assumption: If the expiration date is today, the product is not considered expired.
+
+
+        LocalDate today = LocalDate.now();
+
+        return expirationDate.isBefore(today);
+
+
+
+
+    }
+}
