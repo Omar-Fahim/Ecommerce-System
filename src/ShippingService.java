eb300d31c97d7b3f2080c2edde2a795fe5b353d2
Omar Shaaban <98961039+Omar-Fahim@users.noreply.github.com>
2025-07-05 01:34:16 +0300

    Update Shipping Service
    - Added printShippingDetails Method

--- src/ShippingService.java
+++ src/ShippingService.java
@@ -24,6 +24,37 @@ public class ShippingService {
         return (totalWeight/1000) * 1.25;
     }
 
+    private String formatWeight(double grams) {
+        if (grams >= 1000) {
+            return String.format("%.1fkg", grams / 1000.0);
+        } else {
+            return String.format("%.0fg", grams);
+        }
+    }
+
+
+    public void printShippingDetails(List<Shippable> shippableItems) {
+
+        System.out.println("\n** Shipment notice **");
+
+        if (shippableItems.isEmpty()) {
+            System.out.println("No shippable items in the cart.");
+            return;
+        }
+
+        for (Shippable item : shippableItems) {
+            System.out.printf("%dx %-12s %6s\n", ((Product)item).getQuantity(),item.getName(), formatWeight(item.getWeight()));
+        }
+
+        System.out.println("Total package weight " + formatWeight(calculateTotalWeight(shippableItems)));
+
+
+
+    }
+
+
+
+
 
 
 }
