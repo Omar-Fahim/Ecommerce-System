9c864c43e173e24e9b0013f0e04b208706344f81
Omar Shaaban <98961039+Omar-Fahim@users.noreply.github.com>
2025-07-05 02:00:22 +0300

    Update Customer
    - Fixed an error in customer class.
    - Removed Cart from the constructor inputs

--- src/Customer.java
+++ src/Customer.java
@@ -8,7 +8,7 @@ public class Customer {
     private double balance;
     private Cart cart;
 
-    public Customer(String name, double balance, Cart cart) {
+    public Customer(String name, double balance) {
         this.name = name;
         this.balance = balance;
         this.cart = new Cart();
