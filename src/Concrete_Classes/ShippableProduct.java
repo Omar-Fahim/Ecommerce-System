bc85b423ec7beeb6ed3e431461e6fd593f798973
Omar Shaaban <98961039+Omar-Fahim@users.noreply.github.com>
2025-07-05 19:06:21 +0300

    Update Shippable Product
    Fixed error in Shippable product, shippable interface was not implemented

--- src/Concrete_Classes/ShippableProduct.java
+++ src/Concrete_Classes/ShippableProduct.java
@@ -2,13 +2,24 @@ package Concrete_Classes;
 import Models.Product;
 import Interfaces.Shippable;
 
-public class ShippableProduct extends Product {
+public class ShippableProduct extends Product implements Shippable {
     private double weight;
 
 
     public ShippableProduct(String name, double price, int quantity, double weight) {
         super(name, price, quantity);
         this.weight = weight;
+    }
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    @Override
+    public double getWeight() {
+        return weight;
     }
 
 }
