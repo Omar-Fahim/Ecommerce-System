ee8a070c953b7b3970af5ae93d80d16875ed6828
Omar Shaaban <98961039+Omar-Fahim@users.noreply.github.com>
2025-07-05 17:15:21 +0300

    Builder
    Added the Builder Design Pattern as Products may have optional fields

--- src/Builders/ProductBuilder.java
+++ src/Builders/ProductBuilder.java
@@ -1,29 +1,54 @@
-package Factories;
+package Builders;
 
-import Concrete_Classes.ExpiringProduct;
+import Models.Product;
 import Concrete_Classes.NormalProduct;
+import Concrete_Classes.ExpiringProduct;
 import Concrete_Classes.ShippableProduct;
 import Concrete_Classes.Shippable_and_ExpirableProduct;
-import Models.Product;
 
 import java.time.LocalDate;
 
-public class ProductFactory {
+public class ProductBuilder {
+    private String name;
+    private double price;
+    private int quantity;
+    private Double weight;
+    private LocalDate expirationDate;
 
-    public static Product createNormalProduct(String name, double price, int quantity) {
-        return new NormalProduct(name, price, quantity);
+    public ProductBuilder setName(String name) {
+        this.name = name;
+        return this;
     }
 
-    public static Product createExpiringProduct(String name, double price, int quantity, LocalDate expirationDate) {
-            return new ExpiringProduct(name, price, quantity, expirationDate);
+    public ProductBuilder setPrice(double price) {
+        this.price = price;
+        return this;
     }
 
-    public static Product createShippableProduct(String name, double price, int quantity, double weight) {
-            return new ShippableProduct(name, price, quantity, weight);
+    public ProductBuilder setQuantity(int quantity) {
+        this.quantity = quantity;
+        return this;
     }
 
-    public static Product createShippableExpirableProduct(String name, double price, int quantity, double weight, LocalDate expirationDate) {
+    public ProductBuilder setWeight(double weight) {
+        this.weight = weight;
+        return this;
+    }
+
+    public ProductBuilder setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+        return this;
+    }
+
+    public Product build() {
+        if (weight != null && expirationDate != null) {
             return new Shippable_and_ExpirableProduct(name, price, quantity, weight, expirationDate);
+        } else if (weight != null) {
+            return new ShippableProduct(name, price, quantity, weight);
+        } else if (expirationDate != null) {
+            return new ExpiringProduct(name, price, quantity, expirationDate);
+        } else {
+            return new NormalProduct(name, price, quantity);
+        }
     }
-
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,4 +1,4 @@
-import Factories.ProductFactory;
+import Builders.ProductBuilder;
 import Models.Customer;
 import Models.Product;
 
@@ -10,8 +10,20 @@ public class Runner {
     public static void main(String []args) {
 
         Customer c1 = new Customer("Omar Shaaban", 15000);
-        Product p1 = ProductFactory.createShippableExpirableProduct("Cheese", 100, 5,  200,LocalDate.of(2025, 7, 30));
-        Product p2 = ProductFactory.createShippableExpirableProduct("Biscuits", 150, 5, 700,LocalDate.of(2025, 7, 30));
+        Product p1 = new ProductBuilder()
+                .setName("Cheese")
+                .setPrice(100)
+                .setQuantity(5)
+                .setWeight(200)
+                .setExpirationDate(LocalDate.of(2025, 7, 30))
+                .build();
+        Product p2 = new ProductBuilder()
+                .setName("Biscuits")
+                .setPrice(150)
+                .setQuantity(5)
+                .setWeight(700)
+                .setExpirationDate(LocalDate.of(2025, 7, 30))
+                .build();
 
         c1.addToCart(p1, 2);
         c1.addToCart(p2, 1);
